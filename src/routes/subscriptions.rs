608b2515b7e7a8b612c9187953da1d776f9bc1b3
Justin Thurman <justin.thurman@bevy.com>
2023-03-28 09:51:14 -0400

    Move subscriber tracing into decorating macro

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,18 +11,19 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, connection_pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name,
-    );
-    let _request_span_guard = request_span.enter();
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
     let query = sqlx::query!(
         r#"
@@ -38,19 +39,9 @@ pub async fn subscribe(
     .instrument(query_span)
     .await;
     match query {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }

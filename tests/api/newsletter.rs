356b8b0538067b7c97068677ac5c87d3b5b6934b
Justin Thurman <justin.thurman@bevy.com>
2023-04-15 12:49:54 -0400

    Parse newsletter body data; unit tests

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -64,6 +64,42 @@ async fn newsletters_are_delivered_to_confirmed_subscirbers() {
     assert_eq!(response.status().as_u16(), 200);
 }
 
+#[tokio::test]
+async fn newsletters_returns_400_for_invalid_data() {
+    // arrange
+    let app = spawn_app().await;
+    let test_cases = vec![
+        (
+            serde_json::json!({
+                "content": {
+                    "text": "Valid",
+                    "html": "<p>Valid</p>",
+                }
+            }),
+            "missing title",
+        ),
+        (serde_json::json!({"title": "Valid"}), "missing content"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // act
+        let response = reqwest::Client::new()
+            .post(&format!("{}/newsletters", app.address))
+            .json(&invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 when the payload was {}",
+            error_message
+        );
+    }
+}
+
 /// Using the public API of app under test to create unconfirmed subscriber
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = "name=test&email=test%40email.com";

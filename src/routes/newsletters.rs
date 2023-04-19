d2b3f9574e5880b458e2356460985f02477df464
Justin Thurman <justin.thurman@bevy.com>
2023-04-19 10:17:35 -0400

    Refactor to store passwords in PHC string format

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -5,10 +5,9 @@ use actix_web::http::header::HeaderMap;
 use actix_web::http::{header, StatusCode};
 use actix_web::{web, HttpRequest, HttpResponse, ResponseError};
 use anyhow::Context;
-use argon2::{Algorithm, Argon2, Params, PasswordHasher, Version};
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
 use base64::Engine;
 use secrecy::{ExposeSecret, Secret};
-use sha3::Digest;
 use sqlx::PgPool;
 
 use crate::domain::SubscriberEmail;
@@ -180,16 +179,9 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let hasher = Argon2::new(
-        Algorithm::Argon2id,
-        Version::V0x13,
-        Params::new(15000, 2, 1, None)
-            .context("Failed to build Argon2 parameters")
-            .map_err(PublishError::UnexpectedError)?,
-    );
     let row: Option<_> = sqlx::query!(
         r#"
-        SELECT user_id, password_hash, salt
+        SELECT user_id, password_hash
         FROM users
         WHERE username = $1
         "#,
@@ -200,22 +192,26 @@ async fn validate_credentials(
     .context("Failed to perform a query to retrieve stored credentials")
     .map_err(PublishError::UnexpectedError)?;
 
-    let (expected_password_hash, user_id, salt) = match row {
-        Some(row) => (row.password_hash, row.user_id, row.salt),
+    let (expected_password_hash, user_id) = match row {
+        Some(row) => (row.password_hash, row.user_id),
         None => return Err(PublishError::AuthError(anyhow::anyhow!("Unknown username"))),
     };
 
-    let password_hash = hasher
-        .hash_password(credentials.password.expose_secret().as_bytes(), &salt)
-        .context("Failed to hash password")
+    // PasswordHash implements UHC string format, which encodes the hashing algorithm, the algo version,
+    // the algo load parameters, the hash, and the salt; this makes it easy to refactor our code to update
+    // any of these values in the future. We can migrate old user hashes because the old configuration is
+    // encoded in the hash itself.
+    let expected_password_hash = PasswordHash::new(&expected_password_hash)
+        .context("Failed to parse hash in PHC string format.")
         .map_err(PublishError::UnexpectedError)?;
 
-    // lowercase hexadecimal encoding
-    let password_hash = format!("{:x}", password_hash.hash.unwrap());
+    Argon2::default()
+        .verify_password(
+            credentials.password.expose_secret().as_bytes(),
+            &expected_password_hash,
+        )
+        .context("Invalid password.")
+        .map_err(PublishError::AuthError)?;
 
-    if password_hash != expected_password_hash {
-        Err(PublishError::AuthError(anyhow::anyhow!("Invalid password")))
-    } else {
-        Ok(user_id)
-    }
+    Ok(user_id)
 }

04082fe62c49e6b74149ff8c0fc784c06ead876a
Justin Thurman <justin.thurman@bevy.com>
2023-04-25 10:28:57 -0400

    Implement flash message for authentication failed

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,15 +1,15 @@
-use actix_web::cookie::Cookie;
-use actix_web::http::header::ContentType;
-use actix_web::{HttpRequest, HttpResponse};
+use std::fmt::Write;
 
-pub async fn login_form(request: HttpRequest) -> HttpResponse {
-    let error_html: String = match request.cookie("_flash") {
-        None => "".to_string(),
-        Some(cookie) => {
-            format!("<p><i>{}</i></p>", cookie.value())
-        }
-    };
-    let mut response = HttpResponse::Ok()
+use actix_web::http::header::ContentType;
+use actix_web::HttpResponse;
+use actix_web_flash_messages::{IncomingFlashMessages, Level};
+
+pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
+    let mut error_html = String::new();
+    for message in flash_messages.iter().filter(|m| m.level() == Level::Error) {
+        writeln!(error_html, "<p><i>{}</i></p>", message.content()).unwrap();
+    }
+    HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
@@ -39,10 +39,5 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
     </form>
 </body>
 </html>"#,
-        ));
-    // reset the cookie with a max age of 0 to immediately expire, clearing it after we have the error html
-    response
-        .add_removal_cookie(&Cookie::new("_flash", ""))
-        .unwrap();
-    response
+        ))
 }

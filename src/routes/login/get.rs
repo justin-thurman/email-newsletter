a2c136f43f7ac218fe16af59d888b7718a6dba97
Justin Thurman <justin.thurman@bevy.com>
2023-04-25 09:57:16 -0400

    Add login get html

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -2,7 +2,36 @@ use actix_web::http::header::ContentType;
 use actix_web::HttpResponse;
 
 pub async fn login_form() -> HttpResponse {
+    let error_html: String = "temp".to_string();
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("login.html"))
+        .body(format!(
+            r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Login</title>
+</head>
+<body>
+    {error_html}
+    <form action="/login" method="post">
+        <label>Username
+            <input
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+            >
+        </label>
+        <label>Password
+            <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+            >
+        </label>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>"#,
+        ))
 }

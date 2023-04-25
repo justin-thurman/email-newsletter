e92ad99e3a96f3f9032f48d5a496bcdc7a4ce0db
Justin Thurman <justin.thurman@bevy.com>
2023-04-25 10:46:24 -0400

    WIP redis store config

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,5 @@
+use actix_session::storage::RedisSessionStore;
+use actix_session::SessionMiddleware;
 use std::net::TcpListener;
 
 use actix_web::cookie::Key;
@@ -24,7 +26,7 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
 
         let sender_email = configuration
@@ -51,7 +53,9 @@ impl Application {
             email_client,
             configuration.application.base_url,
             configuration.application.hmac_secret,
-        )?;
+            configuration.redis_uri,
+        )
+        .await?;
         Ok(Self { port, server })
     }
 
@@ -75,25 +79,34 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
 // Actix extractors are type-based, so we need a unique type to try to extract.
 pub struct ApplicationBaseUrl(pub String);
 
-fn run(
+async fn run(
     listener: TcpListener,
     connection_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
     hmac_secret: Secret<String>,
-) -> Result<Server, std::io::Error> {
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     let connection_pool = web::Data::new(connection_pool);
     let email_client = web::Data::new(email_client);
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
 
-    // creating a message store for actix-web-flash-messages, signed with our hmac key
-    let message_store =
-        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+
+    // creating a message store for actix-web-flash-messages, signed with our key
+    let message_store = CookieMessageStore::builder(secret_key.clone()).build();
     // build the message framework which will wrap our app
     let message_framework = FlashMessagesFramework::builder(message_store).build();
+
+    // build a redis store for session management through actix-session
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
     let server = HttpServer::new(move || {
         App::new()
             .wrap(message_framework.clone())
+            .wrap(SessionMiddleware::new(
+                redis_store.clone(),
+                secret_key.clone(),
+            ))
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))

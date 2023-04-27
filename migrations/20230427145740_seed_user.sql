6668e1daddc8dc031e1688e7857174dc7a377230
Justin Thurman <justin.thurman@bevy.com>
2023-04-27 11:04:51 -0400

    Seed user data migration

---
+++ migrations/20230427145740_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+        '7f34faba-bea3-4aa4-afc0-0d40b19f3896',
+        'admin',
+        '$argon2id$v=19$m=15000,t=2,p=1$TxLhLphYMbwVE3IKmpa4jQ$63zyIcZbFtMUSieNB5k9PT5wOHJYj6LzDBJrNrUygoc'
+       );

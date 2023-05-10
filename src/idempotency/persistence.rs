3bc7ca711e1bc1ffca5aea943b74bdd4e6ff0f03
Justin Thurman <justin.thurman@bevy.com>
2023-05-10 09:36:44 -0400

    Basic idempotency implementation with saved responses

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -1,5 +1,8 @@
 use crate::idempotency::IdempotencyKey;
+use actix_web::body::to_bytes;
+use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
+use sqlx::postgres::{PgHasArrayType, PgTypeInfo};
 use sqlx::PgPool;
 use uuid::Uuid;
 
@@ -8,6 +11,12 @@ use uuid::Uuid;
 struct HeaderPairRecord {
     name: String,
     value: Vec<u8>,
+}
+
+impl PgHasArrayType for HeaderPairRecord {
+    fn array_type_info() -> PgTypeInfo {
+        PgTypeInfo::with_name("_header_pair")
+    }
 }
 
 pub async fn get_saved_response(
@@ -31,5 +40,59 @@ pub async fn get_saved_response(
     )
     .fetch_optional(pool)
     .await?;
-    todo!()
+    if let Some(record) = saved_response {
+        let status_code = StatusCode::from_u16(record.response_status_code.try_into()?)?;
+        let mut response = HttpResponse::build(status_code);
+        for HeaderPairRecord { name, value } in record.response_headers {
+            response.append_header((name, value));
+        }
+        Ok(Some(response.body(record.response_body)))
+    } else {
+        Ok(None)
+    }
+}
+
+pub async fn save_response(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+    http_response: HttpResponse,
+) -> Result<HttpResponse, anyhow::Error> {
+    let (response_head, body) = http_response.into_parts();
+    // `MessageBody::Error` is not `Send` + `Sync`, so it can't implicitly convert to anyhow::Error
+    let body = to_bytes(body).await.map_err(|e| anyhow::anyhow!("{}", e))?;
+    let status_code = response_head.status().as_u16() as i16;
+    let headers = {
+        let mut headers = Vec::with_capacity(response_head.headers().len());
+        for (name, value) in response_head.headers().iter() {
+            let name = name.as_str().to_owned();
+            let value = value.as_bytes().to_owned();
+            headers.push(HeaderPairRecord { name, value });
+        }
+        headers
+    };
+    sqlx::query_unchecked!(
+        r#"
+        INSERT INTO idempotency (
+            user_id,
+            idempotency_key,
+            response_status_code,
+            response_headers,
+            response_body,
+            created_at
+        )
+        VALUES ($1, $2, $3, $4, $5, now())
+        "#,
+        user_id,
+        idempotency_key.as_ref(),
+        status_code,
+        headers,
+        body.as_ref()
+    )
+    .execute(pool)
+    .await?;
+
+    // `map_into_boxed_body` converts from `HttpResponse<Bytes>` to `HttpResponse<BoxBody>`
+    let http_response = response_head.set_body(body).map_into_boxed_body();
+    Ok(http_response)
 }
--- src/routes/admin/newsletters/post.rs
+++ src/routes/admin/newsletters/post.rs
@@ -4,7 +4,7 @@ use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
 use crate::error_handling::error_chain_fmt;
-use crate::idempotency::IdempotencyKey;
+use crate::idempotency::{get_saved_response, save_response, IdempotencyKey};
 use crate::routes::get_username;
 use crate::routing_helpers::{e400, e500, see_other};
 use actix_web::body::BoxBody;
@@ -80,6 +80,13 @@ pub async fn publish_newsletter(
         idempotency_key,
     } = form.0;
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+    if let Some(response) = get_saved_response(&pool, &idempotency_key, user_id)
+        .await
+        .map_err(e500)?
+    {
+        FlashMessage::info("The newsletter issue has been published!").send();
+        return Ok(response);
+    }
 
     let confirmed_subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
     for subscriber in confirmed_subscribers {
@@ -110,7 +117,11 @@ pub async fn publish_newsletter(
         }
     }
     FlashMessage::info("The newsletter issue has been published!").send();
-    Ok(see_other("/admin/newsletters"))
+    let response = see_other("/admin/newsletters");
+    let response = save_response(&pool, &idempotency_key, user_id, response)
+        .await
+        .map_err(e500)?;
+    Ok(response)
 }
 
 /// Gets all confirmed subscribers
